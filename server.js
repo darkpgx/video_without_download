d2958b839142d8ef2706ce3ea5f86eb8b88b1345
darkpgx <darkpgx@gmail.com>
2014-09-19 17:30:29 -0700

    add environment variable port

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8888;
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
   res.render('index.ejs');
 });
 
-app.listen(8888);
+app.listen(port);
